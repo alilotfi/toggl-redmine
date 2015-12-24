1248624a36166289eef67e7c45ec0232b9e4f562
Ali Lotfi <alilotfi3@gmail.com>
2015-12-25 03:04:19 +0330

    Improve Toggle class structure

--- toggl.py
+++ toggl.py
@@ -1,7 +1,11 @@
+import datetime
+
 import re
 import requests
-from dateutil import parser
+from dateutil import parser, tz
 
+TOGGL_API_URL = 'https://www.toggl.com/api/v8/time_entries'
+TOGGL_AUTH = ('668ef91140905aa11b361b9c473967c0', 'api_token')
 ACTIVITY_CODE = 9
 
 
@@ -15,19 +19,30 @@ class Toggle:
         self.end = end
         self.description = description
 
+    def __str__(self):
+        return '#' + str(self.issue) + ' - ' + self.description
+
     @classmethod
-    def create_toggles(cls):
-        response = requests.get('https://www.toggl.com/api/v8/time_entries',
-                                params={'start_date': '2015-11-14T00:00:00+03:30',
-                                        'end_date': '2015-12-13T23:59:59+03:30'},
-                                auth=('668ef91140905aa11b361b9c473967c0', 'api_token'))
+    def create_toggles(cls, start_date=None, end_date=None):
+        if not end_date:
+            end_date = datetime.datetime.now(tz=tz.tzstr('UTC+03:30'))
+        if not start_date:
+            start_date = end_date - datetime.timedelta(days=30)
+
+        response = requests.get(TOGGL_API_URL,
+                                params={'start_date': start_date.strftime('%Y-%m-%dT%H:%M:%S+03:30'),
+                                        'end_date': end_date.strftime('%Y-%m-%dT%H:%M:%S+03:30')},
+                                auth=TOGGL_AUTH)
 
         times_json = response.json()
         entries = []
         for time in times_json:
-            if time.get('tags') and ('No - PM' in time.get('tags') or 'PM' in time.get('tags')):
-                print('Skipping entry:', time)
-                continue
+            if time.get('tags'):
+                if 'PM' in time.get('tags'):
+                    continue
+                elif 'No - PM' in time.get('tags'):
+                    print('Skipping entry (NO - PM):', time)
+                    continue
 
             description = time.get('description')
             issue_description = re.search('#(?P<issue>\d+) *- *(?P<description>.*)', description)
@@ -35,7 +50,7 @@ class Toggle:
                 issue = issue_description.group('issue')
                 description = issue_description.group('description')
             else:
-                print('Skipping entry:', time)
+                print('Skipping entry (ISSUE):', time)
                 continue
 
             start = parser.parse(time.get('start'))
@@ -51,3 +66,11 @@ class Toggle:
             duration = "%d:%02d" % (h, m)
 
             entries.append(Toggle(issue, duration, date=date, start=start, end=end, description=description))
+
+        return entries
+
+
+items = Toggle.create_toggles()
+print('--- FETCH DONE ---')
+for i in items:
+    print(str(i))
